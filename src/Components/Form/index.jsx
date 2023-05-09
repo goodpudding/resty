9cea301522dfc32159d1f25d3361ee17a5c0c970
Trey Young(goodpudding) <james.trey.young@gmail.com>
2023-05-08 21:31:08 -0700

    phase 2 done

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,34 +1,69 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './Form.scss';
+import "./Form.scss";
 
-function Form(props) {
+function Form({handleApiCall}) {
+  const [url, setUrl] = useState("");
+  const [method, setMethod] = useState("GET");
 
-  let handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const formData = {
-      method:'GET',
-      url: 'https://cats.co/api/v2/cat',
+      method: method,
+      url: url,
     };
-    props.handleApiCall(formData);
-  }
-    return (
-      <>
-        <form onSubmit={handleSubmit}>
-          <label >
-            <span>URL: </span>
-            <input name='url' type='text' />
-            <button type="submit">GO!</button>
-          </label>
-          <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
-        </form>
-      </>
-    );
+    handleApiCall(formData);
+  };
+
+  const handleMethodChange = (method) => {
+    setMethod(method);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>URL: </span>
+          <input
+            name="url"
+            type="text"
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button type="submit">GO!</button>
+        </label>
+        <label className="methods">
+          <span
+            id="get"
+            className={method === "GET" ? "active" : ""}
+            onClick={() => handleMethodChange("GET")}
+          >
+            GET
+          </span>
+          <span
+            id="post"
+            className={method === "POST" ? "active" : ""}
+            onClick={() => handleMethodChange("POST")}
+          >
+            POST
+          </span>
+          <span
+            id="put"
+            className={method === "PUT" ? "active" : ""}
+            onClick={() => handleMethodChange("PUT")}
+          >
+            PUT
+          </span>
+          <span
+            id="delete"
+            className={method === "DELETE" ? "active" : ""}
+            onClick={() => handleMethodChange("DELETE")}
+          >
+            DELETE
+          </span>
+        </label>
+      </form>
+    </>
+  );
 }
 
 export default Form;
--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './Results.scss';
 
 function Results({data}){
     return (
       <section>
+           <Suspense fallback={<Loading />}>
         <pre>{data ? JSON.stringify(data, undefined, 2) : null}</pre>
+        </Suspense>
       </section>
     );
+}
+function Loading() {
+  return <h2>🌀 Loading...</h2>;
 }
 
 export default Results;
